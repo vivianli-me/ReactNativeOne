2ae114d0e846a64b1bed405c1cacc4c807946cb8
lipeiwei <peiwei.szu@gmail.com>
2016-10-08 11:36:42 +0800

    1. 使用正则表达式去除文章详细信息中的html标签 2. reading中的文章detail用了一个Text来显示，在Android平台下没有问题，但是在iOS模拟器上，文本一直没有显示，起初我怀疑是布局的原因，为了去除其它元素的干扰，我已将页面结构简化成一个ScrollView嵌套一个Text了，我查找原因，发现只有将Text的fontSize不断调小直到文本显示出来，而且我发现，如果文本再增多，这个所对应能显示出来的fontSize会更小，我会继续跟进, 解决该问题

--- app/api/reading.js
+++ app/api/reading.js
@@ -5,7 +5,7 @@
 
 import {get} from './apiHelper';
 
-//顶部滚动的图片
+//顶部滚动的图片信息列表
 export function getReadingImageList() {
   return get('/reading/carousel');
 }
@@ -21,29 +21,42 @@ export function getLatestArticleList() {
 }
 
 //essay serialcontent question
+//短篇   连载          问答
 const articleType = ['essay', 'serialcontent', 'question'];
 export function getSpecifiedTypeArticleList(year, month, index) {
   month = month + 1;//程序里月份表示范围是0~11, 所以要加一
   return get(`/${articleType[index]}/bymonth/${year}-${month}`);
 }
 
+const regExp = new RegExp('<[a-zA-Z0-9_/]+>|\r\n|\n', 'g');
+
+const replaceHTMLTag = text => {
+  if (text == '<br>') {
+    return '\n';
+  }
+  return '';
+};
+
 //获取短篇详细信息
 export function getEssayDetailInfo(id) {
-  return get(`/essay/${id}`);
-  /*
-   .then(detailData => {
-   detailData.hp_content = detailData.hp_content.replace(new RegExp('<br>', 'g'), '');//去除掉文本中的<br>
-   return detailData;
-   })
-   */
+  return get(`/essay/${id}`).then(detailData => {
+    detailData.hp_content = detailData.hp_content.replace(regExp, replaceHTMLTag);
+    return detailData;
+  })
 }
 
 //获取连载详细信息
 export function getSerialDetailInfo(id) {
-  return get(`/serialcontent/${id}`);
+  return get(`/serialcontent/${id}`).then(detailData => {
+    detailData.content = detailData.content.replace(regExp, replaceHTMLTag);
+    return detailData;
+  });
 }
 
 //获取问答详细信息
 export function getQuestionDetailInfo(id) {
-  return get(`/question/${id}`);
+  return get(`/question/${id}`).then(detailData => {
+    detailData.answer_content = detailData.answer_content.replace(regExp, replaceHTMLTag);
+    return detailData;
+  });
 }
