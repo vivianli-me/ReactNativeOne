98082550f7c75aa3325fabfb173de60f057d6c65
lipeiwei <544063528@qq.com>
2016-11-14 21:16:00 +0800

    1. 因侵权问题删除项目

--- index.android.js
+++ index.android.js
@@ -3,7 +3,18 @@
 import React from 'react';
 import {
   AppRegistry,
+  Text,
+  View
 } from 'react-native';
-import App from './app';
 
-AppRegistry.registerComponent('ReactNativeOne', App);
+class App extends React.Component {
+  render() {
+    return (
+      <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+        <Text style={{fontSize: 18}}>该项目因私自获取ONE的接口数据, 所以不再维护</Text>
+      </View>
+    );
+  }
+}
+
+AppRegistry.registerComponent('ReactNativeOne', () => App);
